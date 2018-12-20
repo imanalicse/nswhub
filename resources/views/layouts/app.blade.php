3addcb321082aeddf61aadb6cce1495329e9d148
iman <iman@bitmascot.com>
2018-12-20 12:47:53 +0600

    sass run

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{config('app.name')}}</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
+        <title>{{config('app.name')}}</title>
     </head>
     <body>        
        @yield('content')      
