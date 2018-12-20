e4fe1c0d941df1172a662319d0ab5b911aa9eb56
iman <iman@bitmascot.com>
2018-12-20 08:33:31 +0600

    controller and pages

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'PagesController@index');
+Route::get('/customer-list', 'PagesController@customerList');
