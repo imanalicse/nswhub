c02fb8925ba6a2398b6150c7c5be9c407a0ce49e
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2018-12-25 11:20:01 +0600

    category controller for admin

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -7,6 +7,17 @@ use App\Category;
 
 class CategoriesController extends Controller
 {
+
+     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
