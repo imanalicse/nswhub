7f6cecd391c9d44dd7813091c2a9b76630a2f305
DESKTOP-1HKAK02\User <imanali.cse@gmail.com>
2018-12-25 15:25:55 +0600

    admin prefix and auth route

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,32 @@
 
 Route::get('/', 'PagesController@index');
 Route::get('/order-list', 'PagesController@orderList');
-Route::get('/customer-list', 'PagesController@customerList');
+//Route::get('/customer-list', 'PagesController@customerList');
 Route::resource('categories', 'CategoriesController');
 Auth::routes();
 
 Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+
+Route::get('profile', function () {
+    // Only authenticated users may enter...
+    Route::get('/customer-list', 'PagesController@customerList');
+})->middleware('auth');
+
+Route::prefix('admin')->group(function () {
+    Route::middleware(['auth'])->group(function () {
+        Route::get('users', function () {
+            echo "Usersss";
+        });
+    });
+});
+
+Route::group(['prefix' => 'admin',  'middleware' => 'auth'], function()
+{
+    Route::get('/', function() {
+        echo "Dashboard";
+    });
+    
+    Route::get('customer', function() {
+        echo "Customer";
+    });
+});
